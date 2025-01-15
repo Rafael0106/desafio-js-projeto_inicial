f46d993cba6fa290acd5afbc46df89afe9141473
Rafael Sobral Coutinho <125751433+Rafael0106@users.noreply.github.com>
2025-01-15 16:26:05 -0300

    Update app.js
    Desafio do 1 ao 10 lógica e programação JS

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,14 @@
+alert('Boas vindas ao nosso site!');
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let salDisponivel = 1000;
+alert('Erro! Preencha todos os campos');
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+/*let nome = prompt('Qual é o seu nome?');
+let idade = prompt('Digite a sua idade');
+let idade = prompt('Digite a sua idade');
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!'); 
+}*/
